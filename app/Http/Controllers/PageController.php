b86ec50e935115b8253cbe2f62baf4c8ef22229c
Fabio-Bianco <biancobfabio@gmail.com>
2025-09-16 20:08:47 +0200

    fix: remove debug statement and ensure movies are displayed on the homepage

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,7 +11,7 @@ class PageController extends Controller
     {
         $movies = Movie::all();
 
-        dd($movies);  // qui l'esecuzione SI FERMA
+        
 
         return view('homepage', compact('movies'));
     }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,5 +6,12 @@
   <h1 class="h3">Homepage</h1>
    <p>My movies:</p>
 
+   <ul>
+    @foreach ($movies as $movie)
+      <li>{{ $movie->title }}</li>
+      
+    
+    @endforeach
+   </ul>
 
    @endsection
